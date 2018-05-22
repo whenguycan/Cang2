957eed80a1efd91178b98c14a736bf5762dec6f7
jim <jim@cit.com>
2018-05-22 17:06:01 +0800

    增加主页右上角按钮及底部弹出

--- app/src/main/java/com/lepus/cang2/activity/EntranceActivity.java
+++ app/src/main/java/com/lepus/cang2/activity/EntranceActivity.java
@@ -1,8 +1,11 @@
 package com.lepus.cang2.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.lepus.cang2.R;
+import com.lepus.cang2.dialog.BottomDialog;
 
 public class EntranceActivity extends BaseActivity {
 
@@ -11,9 +14,45 @@ public class EntranceActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrance);
 
-        findViewById(R.id.btn_rune).setOnClickListener(view -> startActivity(MainActivity.class));
+        findViewById(R.id.navigator_rune).setOnClickListener(view -> startActivity(MainActivity.class));
 
-        findViewById(R.id.btn_hero).setOnClickListener(view -> startActivity(HeroActivity.class));
+        findViewById(R.id.navigator_hero).setOnClickListener(view -> startActivity(HeroActivity.class));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_more, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(R.id.menu_more == item.getItemId()){
+            showDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showDialog(){
+
+        new BottomDialog.Builder(getContext())
+                .style(R.style.bottomDialog)
+                .layout(R.layout.dialog_bottom)
+                .ids(new int[]{R.id.btn_rune, R.id.btn_hero})
+                .setOnItemClickListener((dialog, view) -> {
+                    switch (view.getId()) {
+                        case R.id.btn_rune:
+                            startActivity(MainActivity.class);
+                            break;
+                        case R.id.btn_hero:
+                            startActivity(HeroActivity.class);
+                            break;
+                    }
+                })
+                .create()
+                .show();
 
     }
 
